aceb03e3069048fa38fab3e762acdb36cd882459
ypag <priyaganadas@priyas-mbp.wv.cc.cmu.edu>
2015-10-08 11:33:07 -0400

    Initial Commit

---
+++ Blueslide1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Blueslide1
+//
+//  Created by Priya Ganadas on 10/8/15.
+//  Copyright © 2015 Priya Ganadas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
